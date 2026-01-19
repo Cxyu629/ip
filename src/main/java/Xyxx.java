6393499b03e2072e36b906bc365c048931f20c6c
Cxyu629 <chowxingyu123@gmail.com>
2026-01-19 15:13:40 +0800

    feat: rename chatbot
    Added:
    - `makeGreetMessage` method
    - `sendMessage` method

---
+++ src/main/java/Xyxx.java
@@ -0,0 +1,29 @@
+public class Xyxx {
+    public static void main(String[] args) {
+        sendMessage(makeGreetMessage());
+    }
+    
+    public static String makeGreetMessage() {
+        String logo = """
+                 \\o       o/                              
+                  v\\     /v                               
+                   <\\   />                                
+                     \\o/    o      o   \\o    o/  \\o    o/ 
+                      |    <|>    <|>   v\\  /v    v\\  /v  
+                     / \\   < >    < >    <\\/>      <\\/>   
+                   o/   \\o  \\o    o/     o/\\o      o/\\o   
+                  /v     v\\  v\\  /v     /v  v\\    /v  v\\  
+                 />       <\\  <\\/>     />    <\\  />    <\\ 
+                               /                          
+                              o                           
+                           __/>                           
+                """;
+        
+        return "Hello from \n \n" + logo + "\nHow may I help you today?";
+    }
+    
+    public static void sendMessage(String message) {
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+}
