4331b92e6d2a27a6297bfc6dbe325fc276519d20
Cxyu629 <chowxingyu123@gmail.com>
2026-01-22 13:37:08 +0800

    test: add text ui test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+REM set java path
+SET PATH=%PATH%;C:\Program Files\Java\jdk-17.0.0.1\bin
+
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
@@ -7,7 +10,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
